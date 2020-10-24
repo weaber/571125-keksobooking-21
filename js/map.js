169ea87222fada8eb5a0d7b1bccbb9b2e0524e9e
weaber <pnyxer@mail.ru>
2020-10-24 20:19:04 +0300

    Добавил reset формы выбора объявления

--- js/map.js
+++ js/map.js
@@ -83,6 +83,7 @@
   };
 
   const disableMapFilters = function () {
+    mapFilters.reset();
     mapFiltersFeatures.disabled = true;
     for (let select of mapFiltersSelects) {
       select.disabled = true;
