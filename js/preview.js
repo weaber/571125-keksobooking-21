5870bbba786ec186310314d27ee5eef072514bae
weaber <pnyxer@mail.ru>
2020-10-27 22:46:51 +0300

    Добавил preview фотографий, и их удаление при успешной загрузке формы

---
+++ js/preview.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const avatarLoadElement = document.querySelector(`.ad-form__field input[type=file]`);
+const avatarPreviewElement = document.querySelector(`.ad-form-header__preview img`);
+const houseLoadElement = document.querySelector(`.ad-form__upload input[type=file]`);
+const housePreviewElement = document.querySelector(`.ad-form__photo`);
+const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
+
+avatarLoadElement.addEventListener(`change`, function () {
+  const file = avatarLoadElement.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some(function (it) {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    let reader = new FileReader();
+    reader.addEventListener(`load`, function () {
+      avatarPreviewElement.src = reader.result;
+    });
+    reader.readAsDataURL(file);
+  }
+});
+
+houseLoadElement.addEventListener(`change`, function () {
+  housePreviewElement.innerHTML = ``;
+  const file = houseLoadElement.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some(function (it) {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    let reader = new FileReader();
+    reader.addEventListener(`load`, function () {
+      let housePhotoPreview = document.createElement(`img`);
+      housePhotoPreview.src = reader.result;
+      housePreviewElement.append(housePhotoPreview);
+    });
+    reader.readAsDataURL(file);
+  }
+});
+
+window.removePreview = function () {
+  avatarPreviewElement.src = `img/muffin-grey.svg`;
+  housePreviewElement.innerHTML = ``;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require(`path`);
 
 module.exports = {
   entry: [
+    `/js/preview.js`,
     `/js/backend.js`,
     `/js/pin.js`,
     `/js/card.js`,
